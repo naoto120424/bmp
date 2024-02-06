35e04dc821d2e6bd9faffb719288f0983f66834f
naoto120424 <naoto-naka@hiroshima-u.ac.jp>
2024-02-07 01:42:04 +0900

    update dct.py

--- dct/dct.py
+++ dct/dct.py
@@ -6,12 +6,9 @@ import matplotlib.pyplot as plt
 
 class DCT:
     def __init__(self, N):
-        self.N = N  # データ数．
-        # 1次元，2次元離散コサイン変換の基底ベクトルをあらかじめ作っておく
+        self.N = N
         self.phi_1d = np.array([self.phi(i) for i in range(self.N)])
 
-        # Nが大きいとメモリリークを起こすので注意
-        # MNISTの28x28程度なら問題ない
         self.phi_2d = np.zeros((N, N, N, N))
         for i in range(N):
             for j in range(N):
@@ -38,50 +35,48 @@ class DCT:
 
     def phi(self, k):
         """離散コサイン変換(DCT)の基底関数"""
-        # DCT-II
         if k == 0:
             return np.ones(self.N) / np.sqrt(self.N)
         else:
             return np.sqrt(2.0 / self.N) * np.cos((k * np.pi / (2 * self.N)) * (np.arange(self.N) * 2 + 1))
-        # DCT-IV(試しに実装してみた)
-        # return np.sqrt(2.0/N)*np.cos((np.pi*(k+0.5)/self.N)*(np.arange(self.N)+0.5))
 
 
 def df(data, alpha, beta, bS=4):
-    l, h, _ = data.shape
-    for i in range(bS - 1, l - (bS - 1), 2):
-        for j in range(h):
-            p3 = data[j, i - 3]
-            p2 = data[j, i - 2]
-            p1 = data[j, i - 1]
-            p0 = data[j, i]
-            q0 = data[j, i + 1]
-            q1 = data[j, i + 2]
-            q2 = data[j, i + 3]
-            q3 = data[j, i + 4]
-            # print(p3, p2, p1, p0, q0, q1, q2, q3)
-            if (abs(p2[0] - p0[0]) < beta) and (abs(p0[0] - q0[0]) < np.round(alpha / 4)):
-                p0t = (p2 + 2 * p1 + 2 * p0 + 2 * q0 + q1 + 4) / 8
-                p1t = (p2 + p1 + p0 + q0 + 2) / 4
-                p2t = (2 * p3 + 3 * p2 + p1 + p0 + q0 + 4) / 8
-            else:
-                p0t = (2 * p1 + p0 + q1 + 2) / 4
-                p1t, p2t = p1, p2
+    l, h, c = data.shape
+    for i in range(l):
+        for j in range(bS - 1, h - bS):
+            for k in range(c):
+                p3 = data[i, j - 3, k]
+                p2 = data[i, j - 2, k]
+                p1 = data[i, j - 1, k]
+                p0 = data[i, j, k]
+                q0 = data[i, j + 1, k]
+                q1 = data[i, j + 2, k]
+                q2 = data[i, j + 3, k]
+                q3 = data[i, j + 4, k]
+                # print(p3, p2, p1, p0, q0, q1, q2, q3)
+                if (abs(p2 - p0) < beta) and (abs(p0 - q0) < np.round(alpha / 4)):
+                    p0t = (p2 + 2 * p1 + 2 * p0 + 2 * q0 + q1 + 4) / 8
+                    p1t = (p2 + p1 + p0 + q0 + 2) / 4
+                    p2t = (2 * p3 + 3 * p2 + p1 + p0 + q0 + 4) / 8
+                else:
+                    p0t = (2 * p1 + p0 + q1 + 2) / 4
+                    p1t, p2t = p1, p2
 
-            if (abs(q2[0] - q0[0]) < beta) and (abs(p0[0] - q0[0]) < np.round(alpha / 4)):
-                q0t = (p1 + 2 * p0 + 2 * q0 + 2 * q1 + q2 + 4) / 8
-                q1t = (p0 + q0 + q1 + q2 + 2) / 4
-                q2t = (2 * q3 + 3 * q2 + q1 + q0 + p0 + 4) / 8
-            else:
-                q0t = (2 * q1 + q0 + p1 + 2) / 4
-                q1t, q2t = q1, q2
+                if (abs(q2 - q0) < beta) and (abs(p0 - q0) < np.round(alpha / 4)):
+                    q0t = (p1 + 2 * p0 + 2 * q0 + 2 * q1 + q2 + 4) / 8
+                    q1t = (p0 + q0 + q1 + q2 + 2) / 4
+                    q2t = (2 * q3 + 3 * q2 + q1 + q0 + p0 + 4) / 8
+                else:
+                    q0t = (2 * q1 + q0 + p1 + 2) / 4
+                    q1t, q2t = q1, q2
 
-            data[j, i - 2] = p2t
-            data[j, i - 1] = p1t
-            data[j, i] = p0t
-            data[j, i + 1] = q0t
-            data[j, i + 2] = q1t
-            data[j, i + 3] = q2t
+                data[i, j - 2, k] = p2t
+                data[i, j - 1, k] = p1t
+                data[i, j, k] = p0t
+                data[i, j + 1, k] = q0t
+                data[i, j + 2, k] = q1t
+                data[i, j + 3, k] = q2t
 
     return data
 
@@ -93,9 +88,10 @@ def mse(original, converted):
 
 def main(q=10, alpha=20, beta=20):
     N = 8
-    dct = DCT(N)  # 離散コサイン変換を行うクラスを作成
+    dct = DCT(N)
 
-    im = np.array(Image.open("lena.bmp"))
+    # im = np.array(Image.open("lena.bmp"))
+    im = np.array(Image.open("my_photo.png"))
     im_c = np.zeros(im.shape)
     im_y = np.zeros(im.shape)
 
@@ -115,34 +111,35 @@ def main(q=10, alpha=20, beta=20):
                 im_y[N * i : N * (i + 1), N * j : N * (j + 1), k] = dct.idct2(im_c[N * i : N * (i + 1), N * j : N * (j + 1), k])
 
     # パラメータα, βによるDe-blocking Filter
-    im_y_db = df(im_y, alpha=alpha, beta=beta)
+    im_y_df = im_y.copy()
+    im_y_df = df(im_y_df, alpha=alpha, beta=beta)
 
     # 元画像と変換画像間のMSEを計算する
-    im_mse = mse(im, im_y.astype(int))
-    im_df_mse = mse(im, im_y_db.astype(int))
+    im_y_mse = mse(im, im_y.astype(int))
+    im_y_df_mse = mse(im, im_y_df.astype(int))
     print(f"Q: {q}, alpha: {alpha}, beta: {beta}")
-    print(f"Quantized : {im_mse}")
-    print(f"De-Blocked: {im_df_mse}")
+    print(f"  Quantized : {im_y_mse}")
+    print(f"  De-Blocked: {im_y_df_mse}")
 
     # 元の画像と復元したものを表示、保存
-    result_path = "result"
+    result_path = "my_res"
     os.makedirs(result_path, exist_ok=True)
     os.makedirs(os.path.join(result_path, f"q{q}"), exist_ok=True)
 
-    fig = plt.figure(figsize=(8, 4))
+    fig = plt.figure(figsize=(15, 4))
     axes = fig.subplots(1, 3)
 
     axes[0].imshow(im)
     axes[1].imshow(im_y.astype(int))
-    axes[2].imshow(im_y.astype(int))
+    axes[2].imshow(im_y_df.astype(int))
 
     axes[0].set_title("original")
-    axes[1].set_title(f"compressed\nmse={im_mse:.2f}")
-    axes[2].set_title(f"de-blocked\nmse={im_df_mse:.2f}")
+    axes[1].set_title(f"compressed\nmse={im_y_mse:.3f}")
+    axes[2].set_title(f"de-blocked\nmse={im_y_df_mse:.3f}")
 
     plt.suptitle(f"Q={q}, alpha={alpha}, beta={beta}")
     plt.tight_layout()
-    plt.savefig(os.path.join(result_path, f"q{q}", f"alpha{alpha}_beta{beta}.jpg"))
+    plt.savefig(os.path.join(result_path, f"q{q}", f"alpha{alpha}_beta{beta}.jpg"), dpi=500)
     plt.close()
 
 
